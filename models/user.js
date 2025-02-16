e5e351cfeda4f4f0781d48de31f80900340688d1
Mohammad Wasiq Khan <mohdwasiqkhan123@gmail.com>
2025-02-17 02:33:49 +0530

    Adding Project Files

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+// In this schema we want username, email and password.
+//Note : Passport Local Mongoose automatically adds the field of username and 
+// password, whether we mention them or not.
+
+const userSchema = new Schema ({
+    email : {
+        type : String,
+        required : true
+    },
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
+
