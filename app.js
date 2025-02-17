b006c8e668f83fb28f96547846f10f6d24ad49ba
Mohammad Wasiq Khan <mohdwasiqkhan123@gmail.com>
2025-02-17 14:49:12 +0530

    adding file

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLASDB_URL;
+const Mongo_URL = "mongodb://127.0.0.1:27017/wanderlust"
+// const dbUrl = process.env.ATLASDB_URL;
 
 main()
     .then(() => {
@@ -31,7 +32,7 @@ main()
     });
 
 async function main() {
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(Mongo_URL);
 };
 
 app.set("view engine", "ejs");
@@ -41,20 +42,19 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
-    touchAfter: 24*60*60
-});
+// const store = MongoStore.create({
+//     mongoUrl: dbUrl,
+//     crypto: {
+//         secret: process.env.SECRET,
+//     },
+//     touchAfter: 24*60*60
+// });
 
-store.on("error", () => {
-    console.log("Error in MongoDB store!!", err);
-});
+// store.on("error", () => {
+//     console.log("Error in MongoDB store!!", err);
+// });
 
 const sessionOptions = {
-    store,
         secret: process.env.SECRET,
     resave : false,
     saveUninitialized : true,
